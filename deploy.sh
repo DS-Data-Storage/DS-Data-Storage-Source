0cab4eb05604f4c6c98675a38d128e38671ff485
DS-Data-Storage <1343768408@qq.com>
2025-09-15 22:11:00 +0800

    自动部署: 2025-09-15 22:11:00

--- deploy.sh
+++ deploy.sh
@@ -10,19 +10,22 @@ git pull origin main
 echo "正在生成静态文件..."
 hexo clean
 hexo generate
+echo "正在运行hexo d"
+hexo d
+echo "部署完成!"
 
 # 添加所有文件
 git add .
 
 # 提交更改
-echo "正在提交更改..."
+echo "正在提交源码更改..."
 git commit -m "自动部署: $(date +"%Y-%m-%d %H:%M:%S")"
 
 # 推送到 GitHub
-echo "正在推送到 GitHub..."
+echo "正在将源码推送到 GitHub..."
 git push origin main
 
 # 切换回开发分支
 git checkout develop
 
-echo "部署完成!"
+echo "源码上传完成!"
